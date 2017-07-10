26b6ba2e8130f9fdeeab1de2354e4da89346c5ac
DavHer <dav93herdoiza93@gmail.com>
2017-07-10 17:54:21 -0600

    Update main.c
    Formato

--- payload_final/source/main.c
+++ payload_final/source/main.c
@@ -14,18 +14,18 @@ void PowerOff()
 
 int main()
 {
-	int i;
+    int i;
     bool use_top = true;
-	char *texto[] = {	"POC Seguridad en el proceso de arranque",
+    char *texto[] = {	"POC Seguridad en el proceso de arranque",
 						"para sistemas embebidos cerrados",
 						" ",
 						"Estudiantes: David Herdoiza",
 						"             Neil Garcia Vargas",
 						"7/7/2017"};
 	
-	while(true){
-		for(i=0; i<6; i++)
-			DrawStringF(START_X, START_Y + (i*10), use_top, texto[i]);
+    while(true){
+	for(i=0; i<6; i++)
+            DrawStringF(START_X, START_Y + (i*10), use_top, texto[i]);
         if (InputWait() & (BUTTON_B | BUTTON_START))
             break;
 	}
